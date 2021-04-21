db189ffd8ff66e53cc43cf8bda3b750fe168a1e1
zimmygee <55589891+zimmygee@users.noreply.github.com>
2021-04-21 10:29:56 -0400

    Update nodeserver.js

--- nodeserver.js
+++ nodeserver.js
@@ -1,7 +1,6 @@
 var http = require('http');
 var fs = require('fs');
 
-const PORT=8080; 
 
 fs.readFile('./index.html', function (err, html) {
 
@@ -11,5 +10,5 @@ fs.readFile('./index.html', function (err, html) {
         response.writeHeader(200, {"Content-Type": "text/html"});  
         response.write(html);  
         response.end();  
-    }).listen(PORT);
+    }).listen(process.env.PORT || 5000);
 });
